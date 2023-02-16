407a7506f0c50f35831cf1a1a9c5f0d81a715faa
Lazolia <zobuzuzi@gmail.com>
2023-02-16 09:36:38 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ *
+ * Description - This is a program that prints a line with the printf function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	return (0);
+}
