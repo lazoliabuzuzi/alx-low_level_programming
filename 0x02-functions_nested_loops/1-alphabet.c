4c0907ba339b1dddccd84db5c83d75aef7e02423
Lazolia <zobuzuzi@gmail.com>
2023-02-21 11:19:32 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print_alphabet - printthe alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+		_putchar(letter);
+
+	_putchar('\n');
+}
