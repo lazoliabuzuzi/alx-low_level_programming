b01083f0a6c8e6dd3804530e22e2c0bc65078738
Lazolia <zobuzuzi@gmail.com>
2023-03-01 14:04:15 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizaes all words of a string
+ * cap_string - capitalizes all words of a string
  * @str: the string to be capitalized
  *
  * Return: a pointer to the changed string
