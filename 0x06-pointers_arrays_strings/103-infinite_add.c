ea93f845d3ea4881b1d90c507e2cdce0a3325d04
Lazolia <zobuzuzi@gmail.com>
2023-03-02 11:18:35 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,11 +14,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
  *         If r cannot store the sum - 0.
  */
 
-char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
 	int num, tens = 0;
 
-	for (; *n1 && *n2; n1 --, n2--, r_index--)
+	for (; *n1 && *n2; n1--, n2--, r_index--)
 	{
 		num = (*n1 - '0') + (*n2 - '0');
 		num += tens;
