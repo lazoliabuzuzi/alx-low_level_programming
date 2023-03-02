e7212ca77749eb706778b2a358afae536a434996
Lazolia <zobuzuzi@gmail.com>
2023-03-02 10:50:06 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,13 +5,13 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 /**
  * add_strings - Adds the numbers stored in two strings.
- * @n1: The strong containing the first number to be added.
- * @n2: The string contasining the second number to be added.
+ * @n1: The string containing the first number to be added.
+ * @n2: The string containing the second number to be added.
  * @r: The buffer to store the result.
  * @r_index: The current index of the buffer.
  *
  * Return: If r can store the sum - a pointer to the result.
- * If r cannot store the sum - 0.
+ *	   If r cannot store the sum - 0.
  */
 
 char *add_strings(char *n1, char *n2, char *r, int r_index);
