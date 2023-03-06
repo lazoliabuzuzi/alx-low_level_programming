33d7b4ce4fb586829c26e8021fac348d899ef9b7
Lazolia <zobuzuzi@gmail.com>
2023-03-06 07:28:21 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,15 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stddef.h>
 
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *haystack, char *needle);
 void print_chessbopard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-void set-string(char **s, char *to);
+void set_string(char **s, char *to);
 
 #endif
