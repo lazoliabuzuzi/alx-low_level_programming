90ceff0261d6a0c7468e797cf9c23d744a34dde8
Lazolia <zobuzuzi@gmail.com>
2023-03-06 11:32:09 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,10 +3,10 @@
 /**
  * print_chessboard - prints the chessboard
  * @a: the row of the array
- * Return: Always 0 (Success)
+ * Return: nothing
  */
 
-void print_chessboard(char (*a)[8]);
+void print_chessboard(char (*a)[8])
 {
 	int i;
 	int j;
