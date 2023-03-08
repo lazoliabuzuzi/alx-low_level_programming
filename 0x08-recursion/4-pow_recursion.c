d0b5c3d9d0b8f1a526e63b119b2b4bbdaefd6228
Lazolia <zobuzuzi@gmail.com>
2023-03-08 10:48:07 -0800

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,20 +5,15 @@
  * to the power of y
  * @x: integer
  * @y: integer
- * Return: pow recursion
+ * Return: the value of @x to the power of @y
  */
 
-int _pow_recursion(int x, int y);
+int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-	{
 		return (-1);
-	}
-
-	else if (y != 0)
-	{
-		return (x * _pow_recursion(x, y - 1));
-	}
-	else
+	if (y == 0)
 		return (1);
+
+	return (x * (_pow_recursion(x, y - 1)));
 }
