2f89c236dc15522f504f57d9c9a4eb59e8a7ee02
Lazolia <zobuzuzi@gmail.com>
2023-03-11 12:14:45 -0800

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -31,17 +31,17 @@ int main(int argc, char *argv[])
 			cents -= 25;
 			continue;
 		}
-		if ((cents - 10) >= 10)
+		if ((cents - 10) >= 0)
 		{
 			cents -= 10;
 			continue;
 		}
-		if ((cents - 5) >= 5)
+		if ((cents - 5) >= 0)
 		{
 			cents -= 5;
 			continue;
 		}
-		if ((cents - 2) >= 2)
+		if ((cents - 2) >= 0)
 		{
 			cents -= 2;
 			continue;
