91b9f499b3dab0bb5ef41c4390e2da6de1b1a871
Lazolia <zobuzuzi@gmail.com>
2023-03-11 08:40:58 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,18 +12,20 @@
 
 int main(int argc, char *argv[])
 {
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int result = num1 * num2;
-	{
-		printf("%d\n", result);
-	}
-
-	return (0);
+	int num1 = 0;
+	int num2 = 0;
 
 	if (argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n", num1 * num2);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	return (0);
 }
