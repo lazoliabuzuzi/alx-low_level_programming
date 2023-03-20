e5a41862ff36b33affe78c3cbb82c404ca5da6a3
Lazolia <zobuzuzi@gmail.com>
2023-03-20 07:48:40 -0700

    changed underscore to header and removed excess lines

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H
+#define _DOG_H
 
 /**
  * struct dog - description of the dog
@@ -7,22 +7,18 @@
  * @age: the age of the dog
  * @owner: the name of the dog owner
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 /**
  * dog_t - defines a new name for type struct dog
  */
-
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
