b5ad415f1154d03e9e9982ceb43dec0511c1fcb1
Lazolia <zobuzuzi@gmail.com>
2023-03-20 11:15:52 -0700

    changed owner to name in line 42

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -39,7 +39,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 		if (max_dog->owner == NULL)
 		{
-			free(max_dog->owner);
+			free(max_dog->name);
 			free(max_dog);
 			return (NULL);
 		}
