118b7c952c8dc570e7ef1e56d7d9e05514d070b8
Lazolia Buzuzi <zobuzuzi@gmail.com>
2023-03-30 11:14:28 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
+void first(void) __attribute__ ((constructor));
+
 /**
  * print_message - prints You're beat! and yet, you must allow,
  * \nI bore my house upon my back!\n before the main function is executed.
  * Return: nothing
  */
 
-void print_message(void)
+void first(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
