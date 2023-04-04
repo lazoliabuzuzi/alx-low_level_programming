3f7a2b41c80b78cc53fd8d7a6fce5d09a6c03759
Lazolia Buzuzi <zobuzuzi@gmail.com>
2023-04-04 15:48:24 -0700

    fixed memory leak

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include "stdlib.h"
 
 /**
  * insert_nodeint_at_index - inserts a new node at a given position
@@ -13,13 +14,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *current;
 	unsigned int i;
 
-	new_node = malloc(sizeof(listint_t));
 	if (head == NULL)
 		return (NULL);
 
-	new_node->n = n;
 	if (idx == 0)
 	{
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->n = n;
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
@@ -38,6 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 
+	new_node->n = n;
 	new_node->next = current->next;
 	current->next = new_node;
 
