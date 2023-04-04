f47f44f2004333a08e4b4213f683651556d25769
Lazolia Buzuzi <zobuzuzi@gmail.com>
2023-04-04 04:50:37 -0700

    changed order of lists.h elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t counter = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		counter++;
 	}
 
 	return (counter);
