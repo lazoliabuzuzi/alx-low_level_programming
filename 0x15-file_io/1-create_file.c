bb87e0311f4e02e4c337a303e6c2fc0df4c32804
Lazolia Buzuzi <zobuzuzi@gmail.com>
2023-04-10 13:16:10 -0700

    corrected the syntax and used 0600 permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,26 +10,33 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, bytes_written;
+	int fd, len, val;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_WRONLY | O_CREAT, 0600);
+		if (fd == -1)
+			return (-1);
+		close(fd);
+		return (1);
+	}
 
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-	{
-		bytes_written = write(fd, text_content, strlen(text_content));
+	len = strlen(text_content);
+	val = write(fd, text_content, len);
 
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+	if (val == -1 || val != len)
+	{
+		close(fd);
+		return (-1);
 	}
+
 	close(fd);
 	return (1);
 }
