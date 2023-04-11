245397d0f9da9e3f45174d9ad77988e0607416fd
Lazolia Buzuzi <zobuzuzi@gmail.com>
2023-04-11 09:56:21 -0700

    changed positions of curly braces for betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,44 +15,36 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		exit(97); }
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		exit(98); }
 	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		exit(99); }
 	while ((rd = read(from, buffer, BUFFER_SIZE)) > 0)
 	{
 		if (write(to, buffer, rd) != rd)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-	}
+			exit(99); }}
 	if (rd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		exit(98); }
 	clfr = close(from);
 	if (clfr == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
-		exit(100);
-	}
+		exit(100); }
 	clto = close(to);
 	if (clto == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to);
-		exit(100);
-	}
+		exit(100); }
 	return (0);
 }
