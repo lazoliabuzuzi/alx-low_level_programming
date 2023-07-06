6965560ab9414959f8681d6f7d1c7643acecaf91
lazoliabuzuzi <zobuzuzi@gmail.com>
2023-07-06 14:02:46 -0700

    fixed betty by adding space on line 22

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 
-	index = key_index((const unsigned char*)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
 	current = ht->array[index];
 	while (current != NULL)
